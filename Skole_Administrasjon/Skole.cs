52684d139cb572c214ebf3dbadc6e122e882e178
Salar Amir <salar831@gmail.com>
2023-01-31 15:26:49 +0100

    lagt til Skole super klassen

---
+++ Skole_Administrasjon/Skole.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skole_Administrasjon
+{
+    internal class Skole
+    {
+        private string skoleNavn;
+        private string city;
+        private string country;
+        private int byggeÅr;
+
+        private string navn;
+        private string alder;
+        private int startÅr;
+
+        public Skole(string skoleNavn, string city, string country, int byggeår)
+        {
+            this.skoleNavn = skoleNavn;
+            this.city = city;
+            this.country = country;
+            this.byggeÅr = byggeår;
+        }
+    }
+}
