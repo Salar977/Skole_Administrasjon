a04d94fedd4ea3bdf2eb3e4a5e09f17c0f89eef5
Salar Amir <salar831@gmail.com>
2023-01-31 14:32:43 +0100

    Add project files.

---
+++ Skole_Administrasjon/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Skole_Administrasjon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
