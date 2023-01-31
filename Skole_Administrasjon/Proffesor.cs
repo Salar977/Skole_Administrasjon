ba5a56ddefdffb0d67d97382f1edf6624506a285
Rayan Baban <Rayan.baban@gokstadakademiet.no>
2023-01-31 15:29:44 +0100

    lagt til professor klasse

---
+++ Skole_Administrasjon/Proffesor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skole_Administrasjon
+{
+    internal class Professor
+    {
+        private int alder;
+        private string navn;
+        private string undervisningsFag;
+
+        public Professor(int alder, string navn, string undervisningsFag )
+        {
+            this.alder = alder;
+            this.navn = navn;
+            this.undervisningsFag = undervisningsFag;
+        }
+    }
+}
