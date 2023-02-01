245938811f5425ef9a64fac53c5911d990769887
Idris Ismail <Idris10b@hotmail.com>
2023-02-01 11:11:21 +0100

    Bytta fields til property.

--- Skole_Administrasjon/Student.cs
+++ Skole_Administrasjon/Student.cs
@@ -8,13 +8,13 @@ namespace Skole_Administrasjon
 {
     public class Student
     { 
-            private string Name;
-            private int Age;
-            public Student(string name, int age)
-            {
-                Name = name;
-                Age = age;
-            }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Student(string name, int age)
+        {
+            Name = name; 
+            Age = age;
+        }
     }
 
 }
